3a4eeabb283d6290c3099aa87f51028498aeeffb
romanbishof <romanbishof@gmail.com>
2022-01-26 02:17:57 +0200

    first right commit

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,12 @@
+export const reducer = (posts = [], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return posts;
+        case 'CREATE':
+            return posts;
+
+        
+        default:
+            break;
+    }
+} 
