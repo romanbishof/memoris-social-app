a9303d6715ce19cfe5e0a8791cae847ab9977897
romanbishof <romanbishof@gmail.com>
2022-01-28 15:25:54 +0200

    deploy

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
     margin: '30px 0',
@@ -15,4 +15,10 @@ export default makeStyles(() => ({
   image: {
     marginLeft: '15px',
   },
+  main: {
+    flexDirection: 'row',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: "column-reverse",
+    },
+  }
 }));
